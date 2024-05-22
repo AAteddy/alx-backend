2ddfc82a2ec2f831289d1e5de5fcebf7599f800f
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-22 13:48:54 +0300

    [Error fixed] requirement issues resolved

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -29,7 +29,7 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 class FIFOCache(BaseCaching):
     """FIFO based caching system that
-    inherits from BaseCaching
+    inherits from BaseCaching.
     """
 
     def __init__(self):
@@ -38,7 +38,7 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Assigns to the dictionary self.cache_data
-        the item value for the key key
+        the item value for the key key.
         """
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
@@ -48,7 +48,9 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Returns the value in self.cache_data linked to key."""
+        """Returns the value in self.cache_data
+        linked to key.
+        """
         if key is None or not key:
             return None
         return self.cache_data.get(key)
