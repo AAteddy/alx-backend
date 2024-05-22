4a84ae4edf9caa6cd7eaea69015d3cb607978b52
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-22 14:48:59 +0300

    [LIFO Caching] ready to be tested

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -28,21 +28,29 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """doc"""
+    """LIFO based caching system that
+    inherits from BaseCaching.
+    """
 
     def __init__(self):
         """Initialization"""
         super().__init__()
 
     def put(self, key, item):
-        """doc doc doc"""
+        """Assigns to the dictionary self.cache_data
+        the item value for the key key.
+        """
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                removed = list(self.cache_data.keys())[-1]
-                self.cache_data.pop(removed)
-                print("DISCARD: {}".format(removed))
+                last_key = list(self.cache_data.keys())[-1]
+                self.cache_data.pop(last_key)
+                print("DISCARD: {}".format(last_key))
             self.cache_data[key] = item
 
     def get(self, key):
-        """doc doc doc"""
+        """Returns the value in self.cache_data
+        linked to key.
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
         return self.cache_data.get(key)
