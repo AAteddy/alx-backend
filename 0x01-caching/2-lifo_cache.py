0c54443d353aa49cdef88a87ab1a96b37912090c
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-22 14:37:08 +0300

    [popitem() - usage] to get the last item in the list

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -38,8 +38,8 @@ class LIFOCache(BaseCaching):
         """doc"""
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key = list(self.cache_data.keys())[-1]
-                self.cache_data.pop(last_key)
+                last_key = self.cache_data.popitem()
+                # self.cache_data.pop(last_key)
                 print("DISCARD: {}".format(last_key))
             self.cache_data[key] = item
 
