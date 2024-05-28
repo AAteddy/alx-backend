d4d56ce1514b65cc21555495b619c6820e85a04f
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-28 12:59:04 +0300

    [Fix Error] app.config.from_object(Config)

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -31,8 +31,8 @@ class Config:
 
 
 app = Flask(__name__)
-# app.config.from_object(Config)
-babel = Babel(app, locale_selector=Config)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route("/")
